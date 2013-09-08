0c7033f7e7c9489a81a64504ea145ff45aba02ed
Jared De Blander <jared0x90@gmail.com>
2013-09-07 20:08:04 -0400

    Draw an image on the screen. Added gitignore

---
+++ game4.py
@@ -0,0 +1,40 @@
+import pygame
+import random
+
+class Game(object):
+    def main(self, screen):
+
+        # Load the player sprite
+        player = pygame.image.load('images/kain.png')
+
+        while True:
+            # --------------------------------------------------
+            # Event handling
+            # --------------------------------------------------
+
+            # Look through the event loop
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    return
+                if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                    return
+
+            # --------------------------------------------------
+            # Draw the screen
+            # --------------------------------------------------
+
+            # Create a backgound color
+            screen.fill((200,200,200))
+
+            # Draw the player
+            screen.blit(player, (320, 240))
+
+            # Render the new screen
+            pygame.display.flip()
+
+# Run the game if we are running this file
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((640,480))
+    pygame.display.set_caption("Ayrscott Games")
+    Game().main(screen)
