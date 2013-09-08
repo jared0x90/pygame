e0df933dd1f811787ecbb1f7703bbf97e8bbcc39
Jared De Blander <jared@deblander.org>
2013-09-08 14:30:35 -0400

    Gravity & jumping, more settings.

--- game.py
+++ game.py
@@ -8,26 +8,34 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load('images/kain.png')
         self.rect = pygame.rect.Rect((settings.SCREEN_WIDTH / 2, settings.SCREEN_HEIGHT / 2), self.image.get_size())
         self.pixels_per_second = 200
+        self.resting = False
+        self.dy = 0
 
-    def update(self, partial_seconds, game):
+    def update(self, dt, game):
         # Store our position prior to update in case
         # we need to go back to it
         last = self.rect.copy()
 
         # Process keystrokes
         key = pygame.key.get_pressed()
-        move_amount = int(self.pixels_per_second * partial_seconds)
+        move_amount = int(self.pixels_per_second * dt)
         if key[pygame.K_LEFT]:
             self.rect.x -= move_amount
         if key[pygame.K_RIGHT]:
             self.rect.x += move_amount
-        if key[pygame.K_UP]:
-            self.rect.y -= move_amount
-        if key[pygame.K_DOWN]:
-            self.rect.y += move_amount
+
+        # Jump / gravity
+        if self.resting and key[pygame.K_SPACE]:
+            self.dy = settings.JUMP_POWER
+        self.dy = min(settings.MAX_FALL_SPEED, self.dy + settings.GRAVITY_RATE)
+
+        self.rect.y += self.dy * dt
+
+
 
         # Check if we ran into a wall
         new = self.rect
+        self.resting = False
         for cell in pygame.sprite.spritecollide(self, game.walls, False):
             cell = cell.rect
             if last.right <= cell.left and new.right > cell.left:
@@ -35,15 +43,19 @@ class Player(pygame.sprite.Sprite):
             if last.left >= cell.right and new.left < cell.right:
                 new.left = cell.right
             if last.bottom <= cell.top and new.bottom > cell.top:
+                # We hit or are on the floor. Therefore we are resting
+                self.resting = True
                 new.bottom = cell.top
+                self.dy = 0
             if last.top >= cell.bottom and new.top < cell.bottom:
+                # We hit the ceiling.
                 new.top = cell.bottom            
+                self.dy = 0
 
 class Game(object):
     def main(self, screen):
-        # Create the clock and set framerate
+        # Create the clock
         clock = pygame.time.Clock()
-        framerate = 30
 
         # Load images
         background = pygame.image.load('images/bg.jpg')
@@ -76,7 +88,7 @@ class Game(object):
             #---------------------------------------------------
             # Setup framerate / reduce CPU use, return time in
             # milliseconds since last tick for sprite movement
-            dt = clock.tick(framerate)
+            dt = clock.tick(settings.FRAMERATE)
 
             # Check the event loop
             for event in pygame.event.get():
