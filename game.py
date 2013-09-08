f3b0ab0067e86c7d1c8314a4d868988dcc81f379
Jared De Blander <jared@deblander.org>
2013-09-08 13:53:04 -0400

    Added walls to game. Slight code cleanup

--- game.py
+++ game.py
@@ -8,7 +8,12 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.rect.Rect((400, 300), self.image.get_size())
         self.pixels_per_second = 120
 
-    def update(self, partial_seconds):
+    def update(self, partial_seconds, game):
+        # Store our position prior to update in case
+        # we need to go back to it
+        last_position = self.rect.copy()
+
+        # Process keystrokes
         key = pygame.key.get_pressed()
         move_amount = self.pixels_per_second * partial_seconds
         if key[pygame.K_LEFT]:
@@ -20,19 +25,37 @@ class Player(pygame.sprite.Sprite):
         if key[pygame.K_DOWN]:
             self.rect.y += move_amount
 
+        # If we have run into a wall use our last position
+        for cell in pygame.sprite.spritecollide(self, game.walls, False):
+            self.rect = last_position
+
 class Game(object):
     def main(self, screen):
-        # Create the clock
+        # Create the clock and set framerate
         clock = pygame.time.Clock()
+        framerate = 60
 
         # Load images
         background = pygame.image.load('images/bg.jpg')
 
         # Create sprites
+        # Main sprite group
         sprites = pygame.sprite.Group()
+        # Create player and add to main sprite group
         self.player = Player(sprites)
+        # Create a group called walls
+        self.walls = pygame.sprite.Group()
+        block = pygame.image.load('images/block.png')
+        for x in xrange(0, 800, 32):
+            for y in xrange(0, 600, 32):
+                if x in (0, 800-32) or y in (0, 600-32):
+                    wall = pygame.sprite.Sprite(self.walls)
+                    wall.image = block
+                    wall.rect = pygame.rect.Rect((x, y), block.get_size())
+        # Add walls to the main sprite group
+        sprites.add(self.walls)
 
-        framerate = 60
+        
 
         #---------------------------------------------------
         # MAIN LOOP
@@ -53,7 +76,7 @@ class Game(object):
                     return
 
             # Update sprites using time since last tick
-            sprites.update(dt / 1000.0)
+            sprites.update(dt / 1000.0, self)
 
             #---------------------------------------------------
             # Draw the screen
